ed273180457902a523ffde766a9649eccbaeb2b8
ErmolaevDS <130141007+Ermolaevventaru@users.noreply.github.com>
2025-01-25 05:26:03 +0300

    3111111

--- Bot_1.py
+++ Bot_1.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Указываем номер билда вручную
-BUILD_NUMBER =99  # Поменяйте этот номер вручную при каждом новом билде
+BUILD_NUMBER ="хер вам" # Поменяйте этот номер вручную при каждом новом билде
 
 
 # Функция для обработки команды /start
